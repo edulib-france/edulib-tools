008a83887f3519aff4bc0d6ee1d67585e6ad90e9
HERVE Eric <eric.herve@editions.int>
2016-12-15 13:47:41 +0100

    rename UAI validation function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const removeAccents = require('remove-accents');
 const UAI_ALPHABET = 'ABCDEFGHJKLMNPRSTUVWXYZ';
 
-module.exports.validateUAI = function(uai) {
+module.exports.isValidUAI = function(uai) {
   if (!uai || uai.length !== 8) { return false; }
   var numericalPart = parseInt(uai.substring(0, 7));
   if (isNaN(numericalPart)) { return false; }
