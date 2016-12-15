8febeed47687f69e696d60b28b54695cfb144f38
HERVE Eric <eric.herve@editions.int>
2016-12-15 13:43:45 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const removeAccents = require('remove-accents');
+const UAI_ALPHABET = 'ABCDEFGHJKLMNPRSTUVWXYZ';
+
+module.exports.validateUAI = function(uai) {
+  if (!uai || uai.length !== 8) { return false; }
+  var numericalPart = parseInt(uai.substring(0, 7));
+  if (isNaN(numericalPart)) { return false; }
+  var code = uai.substring(7);
+  return code === UAI_ALPHABET.charAt(numericalPart % 23);
+};
+
+module.exports.slug = function(data) {
+  return data ?
+    removeAccents(data.trim()).toLowerCase()
+    .replace(/ +/g, '_').replace(/'+/g, '') :
+    '';
+};
